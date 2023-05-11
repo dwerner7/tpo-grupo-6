154b688adc7c322d2efc425ad8719d376288f34e
dwerner7 <daiana.werner95@gmail.com>
2023-05-11 15:39:35 -0300

    TP con productos, home y formulario de contacto

---
+++ js/index.js
@@ -0,0 +1,17 @@
+anime({
+    targets: 'h1.sin-margen',
+    translateX: 350,
+    loop: 1,
+    direction: 'reverse',
+    easing: 'easeInOutSine'
+  });
+
+  anime({
+    targets: '.nav-item',
+    translateX: 250,
+    direction: 'alternate',
+    easing: 'easeInOutSine'
+  });
+
+
+  window.alert("Bienvenido a nuestro sitio web");
