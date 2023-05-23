96090dc61cbee327d66c76dc37f585d46ef7ba28
dwerner7 <daiana.werner95@gmail.com>
2023-05-22 23:00:59 -0300

    ...

--- js/productos.js
+++ js/productos.js
@@ -1,3 +1,6 @@
+// let data2=fetch("https://www.googleapis.com/books/v1/volumes?q=harry+potter");
+// let data2=response.json();
+
 let libros=`
 <h2 class="title-productos">Descubre nuestros libros</h2>
 <div class="container-libros">`
@@ -7,13 +10,26 @@ for(let elemento of data){
                     <div class="libro">
                         <img src= ${elemento.portada} alt=${elemento.titulo}>
                         <div class="infoLibro">
-                            <h3> ${elemento.titulo} </h3>
-                            <p> ${elemento.autor}  </p>
-                            <p> ${elemento.precio} </p>
+                            <h3 class="titulo-libro"> ${elemento.titulo} </h3>
+                            <p class="autor-libro"> ${elemento.autor}  </p>
+                            <p class="precio-libro"> ${elemento.precio} </p>
                         </div>
                     </div>
                     `
-}
+                }
+
+// for(let libro of data2){
+    // libros = libros + `       
+    //                 <div class="libro">
+    //                     <img src= ${libro.volumeInfo.imageLinks.thumbnail} alt=${libro.volumeInfo.title}>
+    //                     <div class="infoLibro">
+    //                         <h3 class="titulo-libro"> ${libro.volumeInfo.title} </h3>
+    //                         <p class="autor-libro"> ${libro.volumeInfo.authors[0]}  </p>
+    //                         <p class="precio-libro"> Editorial: ${libro.volumeInfo.publisher} </p> //en realidad iria el precio
+    //                     </div>
+    //                 </div>
+    //                 `
+// }
 libros=libros+`</div>`
 console.log(libros);
 document.querySelector(".main-productos").innerHTML=libros
