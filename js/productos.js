29caf6109a01045e671b9349d68baa67a8eac8dd
dwerner7 <daiana.werner95@gmail.com>
2023-05-26 23:33:23 -0300

    api

--- js/productos.js
+++ js/productos.js
@@ -65,31 +65,67 @@ const getLibrosAxios = async(busqueda) => {
         /*const*/let response = await axios(`https://www.googleapis.com/books/v1/volumes?q=${busqueda}&printType=books`);
         console.log("Libros: ",response);
         console.log("Cantidad de libros encontrados: ", response.data.items.length);
+        const libros=response.data.items;
+        // setDivs(response.data.items.length);
         
-        setDivs(response.data.items.length);
+        //crea los divs de los libros
+        setDivs(libros.length);
 
+        //para cuando encuentra que a algun libro le falta info no deje un espacio vacio
+        let contadorLibros=1;
         // for(const libro of response.data.items){
-            for (let i = 1; i<=response.data.items.length;i++){
-            const resLibro = await axios(response.data.items[i].selfLink);
+            // for (let i = 1; i<=response.data.items.length;i++){
+                for (let i = 1; i<=libros.length;i++){
+            // const resLibro = await axios(response.data.items[i].selfLink);
             // const resLibro = await axios(libro.selfLink);
             // console.log(resLibro);
             // console.log(resLibro.data.volumeInfo.imageLinks.thumbnail);
             // console.log(resLibro.data.volumeInfo.title);
             // console.log(resLibro.data.volumeInfo.authors[0]);
             
-            const book = `       
+            // const libro = `       
+            // <div class="libro" >
+            //     <img src= ${resLibro.data.volumeInfo.imageLinks.thumbnail} alt=${resLibro.data.volumeInfo.title}>
+            //     <div class="infoLibro">
+            //         <p class="titulo-libro"> ${resLibro.data.volumeInfo.title} </p>
+            //         <p class="autor-libro"> ${resLibro.data.volumeInfo.authors[0]}  </p>
+            //         <h5> ${resLibro.data.volumeInfo.publisher} </h5> 
+            //         <p class="precio-libro">$${random(3000,8000)} </p> 
+            //     </div>
+            // </div>
+            //  `   
+            
+            //validacion
+            if(libros[i]===undefined){continue;}
+
+            //validacion
+            if(libros[i].volumeInfo.imageLinks===undefined){continue;}
+
+            //lo hice para que queda mas prolijo pero no se si hace falta
+            const portada = libros[i].volumeInfo.imageLinks.thumbnail;
+            const titulo = libros[i].volumeInfo.title;
+            const autor = libros[i].volumeInfo.authors[0];
+            const editorial = libros[i].volumeInfo.publisher;
+            
+            //validacion
+            // if(portada===undefined || titulo===undefined || autor===undefined || editorial===undefined){continue;}
+
+            const libro = `       
             <div class="libro" >
-                <img src= ${resLibro.data.volumeInfo.imageLinks.thumbnail} alt=${resLibro.data.volumeInfo.title}>
+                <img src= ${portada} alt=${titulo}>
                 <div class="infoLibro">
-                    <p class="titulo-libro"> ${resLibro.data.volumeInfo.title} </p>
-                    <p class="autor-libro"> ${resLibro.data.volumeInfo.authors[0]}  </p>
-                    <h5> ${resLibro.data.volumeInfo.publisher} </h5> 
+                    <p class="titulo-libro"> ${titulo} </p>
+                    <p class="autor-libro"> ${autor}  </p>
+                    <h5> ${editorial} </h5> 
                     <p class="precio-libro">$${random(3000,8000)} </p> 
                 </div>
             </div>
              `   
 
-            document.querySelector(`#libro-${i}`).innerHTML=  book;
+            // document.querySelector(`#libro-${i}`).innerHTML=  libro;
+            document.querySelector(`#libro-${contadorLibros}`).innerHTML=  libro;
+            
+            contadorLibros++;
         }
 
     } catch (error) {
